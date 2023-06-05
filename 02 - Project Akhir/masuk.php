4e180e622afb53fd1c26487e3e19365300b42d5d
badarkat1 <114754853+badarkat1@users.noreply.github.com>
2023-06-05 22:27:02 +0700

    Update masuk.php

--- 02 - Project Akhir/masuk.php
+++ 02 - Project Akhir/masuk.php
@@ -20,7 +20,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
-        header("location:uas.html");
+        header("location:index.html");
     } else {
         echo "Username atau password salah.";
     }
